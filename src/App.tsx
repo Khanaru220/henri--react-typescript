7f292f1f5aaa471acdf6626cb3391b9c117844da
tin33392js <calsharp0202@yahoo.com>
2023-04-17 17:37:56 +0700

    refactor: implement prop, interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-import { useEffect, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-	const [count, setCount] = useState(0);
+import Message from './Message';
 
-	// const firstName: string;
-	// const firstName: Array<number> = [1, 2, 123];
-	// const aTuple: [string, number] = ['1', 3];
+const initialState = {
+	name: 'Manny',
+	message: 'TypeScript is cool!!',
+};
 
-	enum Codes {
-		first = 1,
-		second,
-		third,
+type State = Readonly<typeof initialState>;
+
+class App extends Component<any> {
+	render() {
+		return (
+			<div className="App">
+				<div>
+					<a href="https://vitejs.dev" target="_blank">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a href="https://reactjs.org" target="_blank">
+						<img src={reactLogo} className="logo react" alt="React logo" />
+					</a>
+				</div>
+				<h1>Vite + React</h1>
+				<div className="card">
+					{/* <button onClick={() => setCount((count) => count + 1)}>
+						count is {count}
+					</button> */}
+				</div>
+				<Message name="Manny" message="This is a simple message" />
+			</div>
+		);
 	}
+}
 
-	const arr: (string | number | undefined)[] = [1, 2, 3, , 4];
-	const func = (): void => {
-		console.log('Warning');
-		return;
-	};
-	useEffect(func);
-
-	let name: any = 'manny';
+/* function App() {
+	const [count, setCount] = useState(0);
 
 	return (
 		<div className="App">
@@ -41,11 +55,9 @@ function App() {
 					count is {count}
 				</button>
 			</div>
-			<p style={{ backgroundColor: 'red' }}>
-				That value {name} is {typeof name} type!
-			</p>
+			<Message name="Manny	" message="This is a simple message" />
 		</div>
 	);
 }
-
+ */
 export default App;
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,1 +1,12 @@
-export default function Message() {}
+interface UserMessage {
+	name: string;
+	message: string;
+}
+
+export default function Message(props: UserMessage) {
+	return (
+		<p>
+			{props.name}, {props.message}
+		</p>
+	);
+}
